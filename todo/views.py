08eede06049449d62c000d300fb492d17d3006f8
4azoff <m.v.chazov@mail.ru>
2021-05-22 19:40:37 +0500

    Реализация

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from todo.models import Todo
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(request):
+    todos = Todo.objects.all()
+    if request.method == "POST":
+        new_todo= Todo(
+            title = request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+    return render(request, 'index.html', {'todos':todos})
+
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect("/")
